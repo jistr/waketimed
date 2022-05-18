9f07f12b5f096c61dcdc08a6de14123d43a221e4
Jiří Stránský <jistr@jistr.com>
2022-05-19 01:17:43 +0200

    test: Push script and config for manual testing on PinePhone

---
+++ scripts/devel-push-to-device.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euo pipefail
+
+PROJECT_DIR=$(realpath "$(dirname $(realpath "$0"))/..")
+WAKETIMED_BUILD_PROFILE=${WAKETIMED_BUILD_PROFILE:-aarch64-unknown-linux-gnu/release}
+BIN_DIR=/opt/waketimed-devel/bin
+CONFIG_DIR=/opt/waketimed-devel/etc
+DIST_DIR=/opt/waketimed-devel/lib
+STATE_DIR=/opt/waketimed-devel/var
+SERVICE_PATH=/etc/systemd/system/waketimed-devel.service
+
+if [ -z "${WAKETIMED_DEVICE_SSH:-}" ]; then
+    echo "Set WAKETIMED_DEVICE_SSH."
+    exit 1
+fi
+
+ssh $WAKETIMED_DEVICE_SSH systemctl stop waketimed-devel || true
+ssh $WAKETIMED_DEVICE_SSH mkdir -p "$BIN_DIR"
+ssh $WAKETIMED_DEVICE_SSH mkdir -p "$CONFIG_DIR"
+ssh $WAKETIMED_DEVICE_SSH mkdir -p "$DIST_DIR"
+ssh $WAKETIMED_DEVICE_SSH mkdir -p "$STATE_DIR"
+scp $PROJECT_DIR/target/$WAKETIMED_BUILD_PROFILE/waketimed $WAKETIMED_DEVICE_SSH:$BIN_DIR/waketimed
+scp $PROJECT_DIR/waketimed/data/devel/config.yaml $WAKETIMED_DEVICE_SSH:$CONFIG_DIR/config.yaml
+scp $PROJECT_DIR/waketimed/data/devel/waketimed-devel.service $WAKETIMED_DEVICE_SSH:$SERVICE_PATH
+rsync -av --delete $PROJECT_DIR/waketimed/data/dist/ $WAKETIMED_DEVICE_SSH:$DIST_DIR
+
+ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwx,g=rX,o=rX "$BIN_DIR"
+ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwX,g=rX,o=rX "$CONFIG_DIR"
+ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwX,g=rX,o=rX "$DIST_DIR"
+ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwX,g=rX,o=rX "$STATE_DIR"
+
+ssh $WAKETIMED_DEVICE_SSH systemctl daemon-reload
