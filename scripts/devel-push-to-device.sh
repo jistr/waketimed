b9884fd789b0b1620ec36c307711d0b17773d6b1
Jiří Stránský <jistr@jistr.com>
2022-10-29 17:26:41 +0200

    dev: Update devel scripts for new config structure

--- scripts/devel-push-to-device.sh
+++ scripts/devel-push-to-device.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 PROJECT_DIR=$(realpath "$(dirname $(realpath "$0"))/..")
 WAKETIMED_BUILD_PROFILE=${WAKETIMED_BUILD_PROFILE:-aarch64-unknown-linux-gnu/release}
 BIN_DIR=/opt/waketimed-devel/bin
-CONFIG_DIR=/opt/waketimed-devel/etc
-DIST_DIR=/opt/waketimed-devel/lib
-STATE_DIR=/opt/waketimed-devel/var
+CONFIG_DIR=/opt/waketimed-devel/config
 SERVICE_PATH=/etc/systemd/system/waketimed-devel.service
 
 if [ -z "${WAKETIMED_DEVICE_SSH:-}" ]; then
@@ -21,8 +19,6 @@ fi
 ssh $WAKETIMED_DEVICE_SSH systemctl stop waketimed-devel || true
 ssh $WAKETIMED_DEVICE_SSH mkdir -p "$BIN_DIR"
 ssh $WAKETIMED_DEVICE_SSH mkdir -p "$CONFIG_DIR"
-ssh $WAKETIMED_DEVICE_SSH mkdir -p "$DIST_DIR"
-ssh $WAKETIMED_DEVICE_SSH mkdir -p "$STATE_DIR"
 scp $PROJECT_DIR/target/$WAKETIMED_BUILD_PROFILE/waketimed $WAKETIMED_DEVICE_SSH:$BIN_DIR/waketimed
 if [ "${WAKETIMED_PUSH_CONFIG:-1}" = 1 ]; then
     scp $PROJECT_DIR/waketimed/data/devel/config.yaml $WAKETIMED_DEVICE_SSH:$CONFIG_DIR/config.yaml
@@ -32,8 +28,6 @@ rsync -av --delete $PROJECT_DIR/waketimed/data/dist/ $WAKETIMED_DEVICE_SSH:$DIST
 
 ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwx,g=rX,o=rX "$BIN_DIR"
 ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwX,g=rX,o=rX "$CONFIG_DIR"
-ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwX,g=rX,o=rX "$DIST_DIR"
-ssh $WAKETIMED_DEVICE_SSH chmod -R u=rwX,g=rX,o=rX "$STATE_DIR"
 
 ssh $WAKETIMED_DEVICE_SSH systemctl daemon-reload
 if [ "$START" = 1 ]; then
