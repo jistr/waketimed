c6f923542504f21c83167409e666e35db9f510fb
Jiří Stránský <jistr@jistr.com>
2023-02-18 14:49:56 +0100

    test: Commit msg linter now fails on incorrect commit range

--- scripts/lint-commit-messages.sh
+++ scripts/lint-commit-messages.sh
@@ -8,6 +8,11 @@ DIR=$(dirname $(realpath $0))
 LINT_COMMITS_RANGE="2346e60e5e23e732240916df98294f48d23b82f7..HEAD"
 
 function lint_commits_in_range() {
+    # If the command wrapped in <() below fails, the outer script
+    # won't. We run it here directly to ensure that it will fail the
+    # lint if the commits range is incorrect.
+    git rev-list --no-merges "$LINT_COMMITS_RANGE" > /dev/null
+
     while read -r rev; do
         lint_commit "$rev"
     done < <(git rev-list --no-merges "$LINT_COMMITS_RANGE")
